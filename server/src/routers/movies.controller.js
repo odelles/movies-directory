46f9bbcf07837968c414c1ad584570d733a038ba
MHQA-138171 <omegameen1@gmail.com>
2022-09-30 00:10:30 +0500

    adding pagination to genre endpoints

--- server/src/routers/movies.controller.js
+++ server/src/routers/movies.controller.js
@@ -8,6 +8,7 @@ const {
     getComedyMovies,
     getThrillerMovies
 } = require('../models/movies.model')
+const { getPagination } = require('../services/query')
 async function httpGetMovieByTitle(req, res) {
     console.log(req.query);
     const title = req.query.title
@@ -30,23 +31,28 @@ async function httpGetMovies(req, res) {
     return res.status(200).json(movies);
 }
 async function httpGetActionMovies(req, res) {
-    const actionMovies = await getActionMovies();
+    const { skip, limit } = getPagination(req.query);
+    const actionMovies = await getActionMovies(skip, limit);
     return res.status(200).json(actionMovies);
 }
 async function httpGetAdventureMovies(req, res) {
-    const adventureMovies = await getAdventureMovies();
+    const { skip, limit } = getPagination(req.query);
+    const adventureMovies = await getAdventureMovies(skip, limit);
     return res.status(200).json(adventureMovies);
 }
 async function httpGetRomanceMovies(req, res) {
-    const romanceMovies = await getRomanceMovies();
+    const { skip, limit } = getPagination(req.query);
+    const romanceMovies = await getRomanceMovies(skip, limit);
     return res.status(200).json(romanceMovies);
 }
 async function httpGetThrillerMovies(req, res) {
-    const thrillerMovies = await getThrillerMovies();
+    const { skip, limit } = getPagination(req.query);
+    const thrillerMovies = await getThrillerMovies(skip, limit);
     return res.status(200).json(thrillerMovies);
 }
 async function httpGetComedyMovies(req, res) {
-    const comedyMovies = await getComedyMovies();
+    const { skip, limit } = getPagination(req.query);
+    const comedyMovies = await getComedyMovies(skip, limit);
     return res.status(200).json(comedyMovies);
 }
 module.exports = {
