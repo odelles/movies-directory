10ca910dae6521597e924014339d71f4777b0e9d
MHQA-138171 <omegameen1@gmail.com>
2022-09-29 23:53:25 +0500

    querying database for movies by genre

--- server/src/models/movies.model.js
+++ server/src/models/movies.model.js
@@ -35,6 +35,27 @@ async function getMovies() {
     return await Movies.find({ rating: { $gte: 7 } }, '-_id -__v')
         .limit(30);
 }
+async function getActionMovies() {
+    return await Movies.find({ 'genres.name': 'Action' }, '-_id -__v')
+        .limit(3);
+}
+async function getThrillerMovies() {
+    return await Movies.find({ 'genres.name': 'Thriller' }, '-_id -__v')
+        .limit(3);
+}
+async function getComedyMovies() {
+    return await Movies.find({ 'genres.name': 'Comedy' }, '-_id -__v')
+        .limit(3);
+}
+async function getRomanceMovies() {
+    return await Movies.find({ 'genres.name': 'Romance' }, '-_id -__v')
+        .limit(3);
+}
+async function getAdventureMovies() {
+    return await Movies.find({ 'genres.name': 'Adventure' }, '-_id -__v')
+        .limit(3);
+}
+
 async function saveMovie(movie) {
     try {
         await Movies.findOneAndUpdate({
@@ -44,7 +65,7 @@ async function saveMovie(movie) {
             poster: movie.poster,
             runtime: movie.runtime,
             genres: JSON.parse(movie.genres.replace(/'/g, '"')),
-            productionCompanies: JSON.parse(movie.genres.replace(/'/g, '"'))
+
         }, {
             overview: movie.overview,
             rating: movie.vote_average,
@@ -52,7 +73,6 @@ async function saveMovie(movie) {
             poster: movie.poster,
             runtime: movie.runtime,
             genres: JSON.parse(movie.genres.replace(/'/g, '"')),
-            productionCompanies: JSON.parse(movie.genres.replace(/'/g, '"'))
 
         }, {
             upsert: true
@@ -67,4 +87,9 @@ module.exports = {
     loadMoviesData,
     getMovies,
     findMovie,
+    getActionMovies,
+    getAdventureMovies,
+    getComedyMovies,
+    getRomanceMovies,
+    getThrillerMovies,
 }
