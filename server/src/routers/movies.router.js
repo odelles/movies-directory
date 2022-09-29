665c1acdd2ab093cf30d42fc34c2a91fc71e7483
MHQA-138171 <omegameen1@gmail.com>
2022-09-29 23:56:53 +0500

    creating genre endpoints

--- server/src/routers/movies.router.js
+++ server/src/routers/movies.router.js
@@ -2,11 +2,21 @@ const express = require('express');
 const {
     httpGetMovieByTitle,
     httpGetMovies,
+    httpGetActionMovies,
+    httpGetComedyMovies,
+    httpGetThrillerMovies,
+    httpGetRomanceMovies,
+    httpGetAdventureMovies,
 } = require('./movies.controller');
 
 const moviesRouter = express.Router();
 
 moviesRouter.get('/movies/title', httpGetMovieByTitle)
 moviesRouter.get('/movies', httpGetMovies)
+moviesRouter.get('/movies/action', httpGetActionMovies)
+moviesRouter.get('/movies/adventure', httpGetAdventureMovies)
+moviesRouter.get('/movies/romance', httpGetRomanceMovies)
+moviesRouter.get('/movies/comedy', httpGetComedyMovies)
+moviesRouter.get('/movies/thriller', httpGetThrillerMovies)
 
 module.exports = moviesRouter;
